037d798d182704a49bb0e70713fc8498f068820f
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-04-18 15:21:31 -0300

    refactor: creating icons folder

--- capputeeno/src/utils/GraphqlFilters.ts
+++ capputeeno/src/utils/GraphqlFilters.ts
@@ -10,8 +10,8 @@ export function getCategoryByType(type: FilterType) {
 
 export function getFieldByType(priority: PriorityTypes) {
     if (priority === PriorityTypes.NEWS) return { field: "created_at", order: "ASC" }
-    if (priority === PriorityTypes.BIGGEST_PRICE) return { field: "price_in_cents", order: "ASC" }
-    if (priority === PriorityTypes.MINOR_PRICE) return { field: "price_in_cents", order: "DSC" }
+    if (priority === PriorityTypes.BIGGEST_PRICE) return { field: "price_in_cents", order: "DSC" }
+    if (priority === PriorityTypes.MINOR_PRICE) return { field: "price_in_cents", order: "ASC" }
 
     return { field: "sales", order: "DSC" }
 }
